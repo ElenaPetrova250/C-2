20a5ea19b27fbc16db0cd0f140980cc99bc6a54b
Elena Petrova <elena.i.11@yandex.ru>
2022-08-09 13:36:01 +0400

    task2

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write ("Введите первое число: ");
+int NumberA = int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int NumberB = int.Parse(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int NumberC = int.Parse(Console.ReadLine());
+int max = int.Parse(Console.ReadLine());
+if (NumberA>NumberB)
+NumberA = max;
+else NumberB = max;
+if (NumberB>NumberC)
+NumberB = max;
+else NumberC = max;
+if (NumberC>NumberA)
+NumberC = max;
+else NumberA = max;
